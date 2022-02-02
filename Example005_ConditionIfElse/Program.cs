9acd62d77b0e7b544b47f85147a628568feb6e91
Andrey Yakunin <a.e.yakunin@gmail.com>
2022-02-02 22:50:45 +0300

    create projekt Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "андрей")
+{
+    Console.WriteLine("Андрей, Здравствуйте!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
