7fe79b1fdeeaf14bb321cce8bc9eb6dc7b0e435f
Andrey Yakunin <a.e.yakunin@gmail.com>
2022-02-06 00:20:36 +0300

    update project TASK 0

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите имя пользователя: ");
-string username = Console.ReadLine();
+string? username = Console.ReadLine();
 
 if (username.ToLower() == "андрей")
 {
---
+++ TASK_0/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести квадрат числа
+
+
+Console.WriteLine("Введите число ->");
+
+string? numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+
+Console.WriteLine($"Квадрат числа: {number * number}");
+
+
+
